842e77f4cf3b4a31e4dd0a1c8bede03060f420f5
Juan Pablo Yamamoto <jpyamamoto@ciencias.unam.mx>
2021-02-28 19:21:43 -0600

    Final version

--- cogs/private_channels.py
+++ cogs/private_channels.py
@@ -9,11 +9,17 @@ class PrivateChannels(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.emails_registrados = set()
+        self.emails_mentores = set()
+
+        with open('data_mentores.csv', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            reader.__next__()
+            self.emails_mentores = set(user[6].lower().strip() for user in reader)
 
         with open('data.csv', encoding='utf-8') as f:
             reader = csv.reader(f)
             reader.__next__()
-            self.emails_registrados = set(user[10] for user in reader)
+            self.emails_registrados = set(user[10].lower().strip() for user in reader)
 
 
     @commands.command()
@@ -91,7 +97,7 @@ class PrivateChannels(commands.Cog):
 
 
     @commands.command()
-    async def usuario(self, ctx, email: str):
+    async def check(self, ctx, email: str):
         if ctx.channel.name != "check-in":
             return
 
@@ -102,9 +108,18 @@ class PrivateChannels(commands.Cog):
             await ctx.send(f"No ingresaste un correo válido, {ctx.author.mention}")
             return
 
-        if email in self.emails_registrados:
+        if email.lower() in self.emails_mentores:
+            role1 = discord.utils.get(ctx.guild.roles, name= 'mentor')
+            role_del = discord.utils.get(ctx.guild.roles, name= 'no-check')
+            await ctx.message.author.add_roles(role1)
+            await ctx.message.author.remove_roles(role_del)
+            channel = discord.utils.get(ctx.guild.channels, name='codigo-de-conducta')
+            await ctx.send(f"Bienvenido {ctx.message.author.mention}! Por favor, dirígete al canal {channel.mention}.")
+        elif email.lower() in self.emails_registrados:
             role = discord.utils.get(ctx.guild.roles, name= 'noauth')
+            role_del = discord.utils.get(ctx.guild.roles, name= 'no-check')
             await ctx.message.author.add_roles(role)
+            await ctx.message.author.remove_roles(role_del)
             channel = discord.utils.get(ctx.guild.channels, name='codigo-de-conducta')
             await ctx.send(f"Bienvenido {ctx.message.author.mention}! Por favor, dirígete al canal {channel.mention}.")
         else:
@@ -120,7 +135,7 @@ class PrivateChannels(commands.Cog):
         for member in members:
             overwrites[member] = discord.PermissionOverwrite(read_messages=True, create_instant_invite=False)
 
-        category = next(c for c in guild.categories if c.name.lower() == 'equipos')
+        category = next(c for c in guild.categories if c.name.lower() == 'equipos2')
 
         channel = await guild.create_text_channel(team, overwrites=overwrites, category=category,
                                                   topic=f"Canal privado para el equipo {team}")
@@ -136,7 +151,7 @@ class PrivateChannels(commands.Cog):
         for member in members:
             overwrites[member] = discord.PermissionOverwrite(read_messages=True, create_instant_invite=False)
 
-        category = next(c for c in guild.categories if c.name.lower() == 'equipos')
+        category = next(c for c in guild.categories if c.name.lower() == 'equipos2')
 
         channel = await guild.create_voice_channel(team, overwrites=overwrites, category=category,
                                                    topic=f"Canal de voz privado para el equipo {team}")
